241c56b9ac1920904e6c05256667daf625d08b59
gerow <gerow@mgerow.com>
2014-02-22 02:44:39 -0800

    reworked the way colors are picked to make sure they contrast well
    it actually turned out somewhat nice! You have a 50% chance of getting a dark
    theme and a 50% chance of getting a light theme. It might be nice to have some
    kind of switch or config file for if you favor one over the other, but overall
    things turned out pretty well!
    
    The main thing I'm concerned about now is that there are more colors that modern
    terminals have set than just foreground and background. I would be surprised if there
    weren't potential clashes with those that make certain colored terminals unusable.
    
    Generating certain color schemes shouldn't be that hard, though, in order to get around
    that. It might be nice to use some kind of prng instead of this hacky crc32 trick in
    order to get seeding datat for these colors...

--- color-ssh.go
+++ color-ssh.go
@@ -16,17 +16,36 @@ func resetColors() {
 	os.Stdout.WriteString("\033]10;7\007")
 }
 
-func hostColor(name string) (uint8, uint8, uint8) {
+func hostColors(name string) (color.HSL, color.HSL) {
 	csum := crc32.ChecksumIEEE([]byte(name))
 
 	log.Printf("Got checksum %d\n", csum)
 
-	var r, g, b uint8
+	var r, g, b, levelmod uint8
+
 	r = uint8(csum)
 	g = uint8(csum >> 8)
 	b = uint8(csum >> 16)
+	levelmod = uint8(csum >> 24)
 
-	return r, g, b
+	foreground := color.RGB{float64(r) / 255, float64(g) / 255, float64(b) / 255}.ToHSL()
+
+	if (csum >> 25) & 0x1 != 0 {
+		// if the 25th bit is set then this is a dark color, otherwise it is light
+		foreground.L = 0.0 + 0.25 * (float64(levelmod) / 255)
+	} else {
+		foreground.L = 0.75 + 0.25 * (float64(levelmod) / 255)
+	}
+
+	// the background should be the complement of the foreground color with the oppisite darkness
+	background := color.HSL{math.Mod(foreground.H + 0.5, 1.0), foreground.S, foreground.L}
+	if foreground.L > 0.5 {
+		background.L = 0.0 + 0.40 * (float64(levelmod) / 255);
+	} else {
+		background.L = 0.60 + 0.40 * (float64(levelmod) / 255);
+	}
+
+	return  foreground, background
 }
 
 func extractHostname(args []string) string {
@@ -47,26 +66,26 @@ func main() {
 
 	cmd := exec.Command("ssh", os.Args[1:]...)
 
-	r, g, b := hostColor(host)
+	fg_hsl, bg_hsl := hostColors(host)
 
-	// Calculate the complement by converting to hsl, rotating h by 180 degrees
-	// and converting back
-	hsl := color.RGB{float64(r) / 255, float64(g) / 255, float64(b) / 255}.ToHSL()
-	hsl.H += 0.5
-	hsl.H = math.Mod(hsl.H, 1.0)
-	complement := hsl.ToRGB()
+	fg := fg_hsl.ToRGB()
+	bg := bg_hsl.ToRGB()
 
-	log.Printf("HSL: %d, %d, %d", hsl.H, hsl.S, hsl.L)
+	bg_r := uint8(bg.R * 255)
+	bg_g := uint8(bg.G * 255)
+	bg_b := uint8(bg.B * 255)
 
-	fg_r := uint8(complement.R * 255)
-	fg_g := uint8(complement.G * 255)
-	fg_b := uint8(complement.B * 255)
+	fg_r := uint8(fg.R * 255)
+	fg_g := uint8(fg.G * 255)
+	fg_b := uint8(fg.B * 255)
 
-	log.Printf("Setting background color to %02x%02x%02x", r, g, b)
-	log.Printf("Setting foreground color to %02x%02x%02x", fg_r, fg_g, fg_b)
+	log.Printf("using bg{%d, %d, %d} fg{%d, %d, %d}", bg_r, bg_b, bg_b, fg_r, fg_b, fg_b)
+
 	// Set the background color
-	fmt.Fprintf(os.Stdout, "\033]11;#%02x%02x%02x\007", r, g, b)
+	fmt.Fprintf(os.Stdout, "\033]11;#%02x%02x%02x\007", bg_r, bg_g, bg_b)
+	// Set the foreground color
 	fmt.Fprintf(os.Stdout, "\033]10;#%02x%02x%02x\007", fg_r, fg_g, fg_b)
+
 	// Set up signals to restore colors when necessary
 	sigc := make(chan os.Signal, 1)
 	signal.Notify(sigc,
